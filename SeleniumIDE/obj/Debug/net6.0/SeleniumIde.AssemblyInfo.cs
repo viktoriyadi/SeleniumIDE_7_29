79fe3fa56ed2567268df88be0d8c9e215c91cb03
viktoriyadi <viksitoyyy@abv.bg>
2024-08-13 19:08:44 +0300

    Initial commit

--- SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
+++ SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73c6a0faaab7d3c658ddbb4f5171e153f18e566")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
